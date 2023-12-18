1fcce2fa6a8e11769345d426343637773cb78015
Tou-u <rodrignacio99@hotmail.com>
2023-12-18 19:27:38 -0300

    init

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { useEffect, useState } from "react";
+
+export default function NavBar() {
+  const [hash, setHash] = useState<string>("");
+
+  useEffect(() => {
+    const handleHashChange = () => setHash(window.location.hash);
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        isBordered
+        classNames={{
+          base: "h-[55px] w-[200px] m-auto",
+          wrapper: "px-0 h-auto",
+          content: "!justify-center",
+        }}
+        className="rounded-full border-2"
+      >
+        <NavbarContent>
+          <NavbarItem isActive={["", "#home"].includes(hash)}>
+            <Link color="foreground" href="#home">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive={hash === "#projects"}>
+            <Link color="foreground" href="#projects">
+              Projects
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+} satisfies Config
