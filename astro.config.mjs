6dd4d9555e158019a9b7e5f4a542eb9e0cdc7f2f
Tou-u <rodrignacio99@hotmail.com>
2023-12-18 19:31:01 -0300

    cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: cloudflare(),
 });
