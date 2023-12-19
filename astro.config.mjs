5834c6218c612f9087739d7327f0a8946440cb00
Tou-u <rodrignacio99@hotmail.com>
2023-12-19 02:39:15 -0300

    no ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
 });
