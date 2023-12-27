801a24589de655bbfaeee899c7526982b2b8d709
Tou-u <rodrignacio99@hotmail.com>
2023-12-26 22:13:01 -0300

    Cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: "static",
+  integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare(),
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,8 @@
-import { nextui } from '@nextui-org/react';
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
-} satisfies Config
+} satisfies Config;
