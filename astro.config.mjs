c97ba62f4682d61676054cbeba945791fd0181a4
Tou-u <rodrignacio99@hotmail.com>
2023-12-29 01:56:23 -0300

    Mejoras de imagenes e iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "server",
   adapter: cloudflare(),
 });
